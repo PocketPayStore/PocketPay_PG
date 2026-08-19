19607e78332212901697f7c6a2605b3eb1b25029
ehddbs4521 <ehddbs452100@naver.com>
2026-08-19 15:07:54 +0900

    feat: add application context smoke test

---
+++ src/test/java/pocketpaystore/pocketpay_pg/PocketPayPgApplicationTests.java
@@ -0,0 +1,13 @@
+package pocketpaystore.pocketpay_pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketPayPgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
